e595153b8386139aba957d8e6bf088dde795e2b2
alexmansonlu <47284461+alexmansonlu@users.noreply.github.com>
2019-05-24 16:24:32 +0100

    hello bonny

--- app/src/main/java/com/example/myapplication/Themes.java
+++ app/src/main/java/com/example/myapplication/Themes.java
@@ -19,7 +19,7 @@ import android.widget.TextView;
 import android.widget.SeekBar;
 import android.widget.Toast;
 ;
-
+//hello motto
 public class Themes extends AppCompatActivity {
     private TextView mTextMessage;
     private static TextView text_view;
