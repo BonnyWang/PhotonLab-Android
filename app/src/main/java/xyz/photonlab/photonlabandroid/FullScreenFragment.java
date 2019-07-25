fa6f11601df77f760f355460cb56b3db21058cd4
kio <2281773918@qq.com>
2019-07-25 12:16:17 +0800

    update animation

--- app/src/main/java/xyz/photonlab/photonlabandroid/FullScreenFragment.java
+++ app/src/main/java/xyz/photonlab/photonlabandroid/FullScreenFragment.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Context;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 import androidx.fragment.app.Fragment;
 
@@ -12,22 +14,32 @@ import androidx.fragment.app.Fragment;
  */
 public class FullScreenFragment extends Fragment {
 
+    private static Animation showAnimation;
+    private static Animation hideAnimation;
+
     private View navBar;
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
         Log.e(context.toString(), "");
+
+        showAnimation = AnimationUtils.loadAnimation(context, R.anim.float_up);
+        hideAnimation = AnimationUtils.loadAnimation(context, R.anim.float_down);
+
         navBar = ((Activity) context).findViewById(R.id.nav_view);
         if (navBar != null) {
             navBar.setVisibility(View.GONE);
+            navBar.startAnimation(hideAnimation);
         }
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
-        if (navBar != null)
+        if (navBar != null) {
             navBar.setVisibility(View.VISIBLE);
+            navBar.startAnimation(showAnimation);
+        }
     }
 }
