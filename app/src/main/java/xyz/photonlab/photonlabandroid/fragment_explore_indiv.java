f0820a3453d877a5c1e2cc8f87b9d14952f360c9
kio <2281773918@qq.com>
2019-07-25 10:28:41 +0800

    title bar adjusted!

--- app/src/main/java/xyz/photonlab/photonlabandroid/fragment_explore_indiv.java
+++ app/src/main/java/xyz/photonlab/photonlabandroid/fragment_explore_indiv.java
@@ -1,22 +1,21 @@
 package xyz.photonlab.photonlabandroid;
 
 import android.annotation.SuppressLint;
-import android.content.Context;
-import android.net.Uri;
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.fragment.app.Fragment;
-
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.fragment.app.Fragment;
 
 
 public class fragment_explore_indiv extends Fragment implements View.OnTouchListener {
@@ -28,6 +27,7 @@ public class fragment_explore_indiv extends Fragment implements View.OnTouchList
     Button btBack;
     TextView tvTitle;
     ConstraintLayout topBox;
+    Button btShare;
 
     public fragment_explore_indiv(String link, String title) {
 
@@ -49,6 +49,7 @@ public class fragment_explore_indiv extends Fragment implements View.OnTouchList
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_explore_indiv, container, false);
         topBox = view.findViewById(R.id.fgexTopBar);
+        btShare = view.findViewById(R.id.button3);
         wvexplore = view.findViewById(R.id.wvexplore);
         wvexplore.loadUrl(link);
         wvexplore.getSettings().setJavaScriptEnabled(true);
@@ -56,12 +57,16 @@ public class fragment_explore_indiv extends Fragment implements View.OnTouchList
         wvexplore.setOnTouchListener(this);
 
         btBack = view.findViewById(R.id.btBackExIndiv);
-        btBack.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                getActivity().getSupportFragmentManager().popBackStack();
-            }
+        btBack.setOnClickListener(v -> getActivity().getSupportFragmentManager().popBackStack());
+
+        btShare.setOnClickListener((v) -> {
+            Intent intent = new Intent(Intent.ACTION_SEND);
+            intent.setType("text/plain");
+            intent.putExtra(Intent.EXTRA_SUBJECT, "PhotonLab Inc");
+            intent.putExtra(Intent.EXTRA_TEXT, wvexplore.getUrl());
+            startActivity(Intent.createChooser(intent,"Share"));
         });
+
 
         tvTitle = view.findViewById(R.id.tvExIndivTitle);
         tvTitle.setText(title);
@@ -88,4 +93,5 @@ public class fragment_explore_indiv extends Fragment implements View.OnTouchList
         }
         return false;
     }
+
 }
